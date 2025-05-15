b1f91d6857a9e38b0da61b6a6367988fa7ff2777
RaajSatya <satyaxcode@gmail.com>
2025-05-15 18:17:45 +0530

    adadsad

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 'use client'
 import { useSession, signOut } from 'next-auth/react';
 import { AppBar, Toolbar, Typography, Button, Stack } from '@mui/material';
+import Link from 'next/link';
 
 export default function Navbar() {
     const { data: session }: any = useSession();
@@ -12,11 +13,18 @@ export default function Navbar() {
                 <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: 'bold' }}>TIANYIN LMS</Typography>
                 {session?.user ? (
                     <Stack direction={'row'} alignItems={'center'} spacing={2}>
+                        <Button color="inherit" component={Link} href="/about">About</Button>
+                        <Button color="inherit" component={Link} href="/contact">Contact</Button>
                         <Typography>{session.user.name} ( {session.user.role} )</Typography>
                         <Button color="inherit" onClick={() => signOut({ redirect: true, callbackUrl: '/login' })}>Logout</Button>
                     </Stack>
                 ) : (
-                    <Button color="inherit" href="/login">Login</Button>
+                    <Stack direction={'row'} alignItems={'center'} spacing={2}>
+                        <Button color="inherit" component={Link} href="/about">About</Button>
+                        <Button color="inherit" component={Link} href="/contact">Contact</Button>
+                        <Button color="inherit" component={Link} href="/login">Login</Button>
+                    </Stack>
+
                 )}
             </Toolbar>
         </AppBar>
