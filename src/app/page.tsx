f49cadc3ab76619b75df1d570a108851a255f907
RaajSatya <satyaxcode@gmail.com>
2025-05-16 15:30:05 +0530

    make this live

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // app/page.tsx
+'use client'
 import { Button, Container, Typography } from '@mui/material';
 import Link from 'next/link';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/auth/authOptions';
 import SignOutButton from '@/components/SignOutButton';
+import { useSession } from 'next-auth/react';
 
-export default async function Home() {
-  const session: any = await getServerSession(authOptions);
+export default function Home() {
+  const { data }: any = useSession()
   return (
     <Container maxWidth="sm" sx={{
       display: 'flex',
@@ -22,64 +22,66 @@ export default async function Home() {
         Welcome to Tianyin&apos;s LMS!
       </Typography>
 
-      {session ? (
-        <>
-          <Typography variant="body1">
-            You&apos;re logged in as: <strong>{session.user.email}</strong>
-          </Typography>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
-            {session.user.role === 'admin' && (
-              <Button
-                variant="contained"
-                size="large"
-                fullWidth
-                component={Link}
-                href="/admin"
-              >
-                Go to Admin Dashboard
-              </Button>
-            )}
-            {session.user.role === 'manager' && (
-              <Button
-                variant="contained"
-                size="large"
-                fullWidth
-                component={Link}
-                href="/manager"
-              >
-                Go to Manager Portal
-              </Button>
-            )}
-            {session.user.role === 'staff' && (
-              <Button
-                variant="contained"
-                size="large"
-                fullWidth
-                component={Link}
-                href="/staff"
-              >
-                Go to Staff Portal
-              </Button>
-            )}
-            <SignOutButton />
-          </div>
-        </>
-      ) : (
-        <>
-          <Typography variant="body1">
-            Please sign in to access your account
-          </Typography>
-          <Button
-            variant="contained"
-            size="large"
-            fullWidth
-            component={Link}
-            href="/login"
-          >
-            Login Now
-          </Button>
-        </>
-      )}
+      {
+
+        data ? (
+          <>
+            <Typography variant="body1">
+              You&apos;re logged in as: <strong>{data.user.email}</strong>
+            </Typography>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+              {data.user.role === 'admin' && (
+                <Button
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  component={Link}
+                  href="/admin"
+                >
+                  Go to Admin Dashboard
+                </Button>
+              )}
+              {data.user.role === 'manager' && (
+                <Button
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  component={Link}
+                  href="/manager"
+                >
+                  Go to Manager Portal
+                </Button>
+              )}
+              {data.user.role === 'staff' && (
+                <Button
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  component={Link}
+                  href="/staff"
+                >
+                  Go to Staff Portal
+                </Button>
+              )}
+              <SignOutButton />
+            </div>
+          </>
+        ) : (
+          <>
+            <Typography variant="body1">
+              Please sign in to access your account
+            </Typography>
+            <Button
+              variant="contained"
+              size="large"
+              fullWidth
+              component={Link}
+              href="/login"
+            >
+              Login Now
+            </Button>
+          </>
+        )}
     </Container>
   );
 }
